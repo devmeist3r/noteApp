3ecd58d248e454da9575d78c671b6bee4bd7545a
devmeist3r <devmeist3r@gmail.com>
2021-10-17 05:51:26 -0300

    *

---
+++ app/src/main/java/com/devmesit3r/noteapp/ui/feature_note/domain/use_case/GetNote.kt
@@ -0,0 +1,12 @@
+package com.devmesit3r.noteapp.ui.feature_note.domain.use_case
+
+import com.devmesit3r.noteapp.ui.feature_note.data.repository.NoteRepository
+import com.devmesit3r.noteapp.ui.feature_note.domain.model.Note
+
+class GetNote(
+    private val repository: NoteRepository
+) {
+    suspend operator fun invoke(id: Int): Note? {
+        return repository.getNoteById(id)
+    }
+}
